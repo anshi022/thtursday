445d5e1215ef32047076315e72ec9a506fce3ff0
anshika <anshikasarathe4@gmail.com>
2026-02-02 13:22:54 +0530

    new project

---
+++ src/main/java/com/example/AJDD/AjddApplication.java
@@ -0,0 +1,13 @@
+package com.example.AJDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AJDD/AjddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AJDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
